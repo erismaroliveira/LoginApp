0ee0abbae2ab78054fe0dc24d3e61650e8200c3e
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-07-21 12:16:20 -0300

    Initial commit

---
+++ LoginApp/LoginApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+
+            MainPage = new NavigationPage(new LoginUI());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ LoginApp/LoginApp/LoginUI.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(txtUsername.Text == "admin" && txtPassword.Text == "123")
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                DisplayAlert("Ops...", "Username or Password is incorrect!", "Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
